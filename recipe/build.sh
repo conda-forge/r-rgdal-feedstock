fa84879d64e8f9298d3c4ca038a4e9f2d8715368
Filipe Fernandes <ocefpaf@gmail.com>
2019-05-12 20:03:14 -0300

    try ACCEPT_USE_OF_DEPRECATED_PROJ_API_H for macOS

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-export LD_LIBRARY_PATH=$PREFIX/lib/:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH=${PREFIX}/lib/:${LD_LIBRARY_PATH}
+
+# The `ACCEPT_USE_OF_DEPRECATED_PROJ_API_H` is a temporary solution and won't work with proj4 7.
+export PKG_CPPFLAGS="-I${PREFIX}/include -DACCEPT_USE_OF_DEPRECATED_PROJ_API_H=1"
+export CFLAGS="-I${PREFIX}/include -DACCEPT_USE_OF_DEPRECATED_PROJ_API_H=1  ${CFLAGS}"
 
 $R CMD INSTALL --build .
